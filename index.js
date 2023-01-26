c341f1783acea4131722bb5905e4f02351aadace
tnguyen606-cs <tnguyen606@gatech.edu>
2023-01-26 00:50:22 -0500

    Added "Even Listener" to the buttons

--- index.js
+++ index.js
@@ -100,10 +100,13 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let listOfGamesUnfunded = GAMES_JSON.filter( (game) => {
+        return game.pledged < game.goal;
+    });
+    console.log(listOfGamesUnfunded);
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(listOfGamesUnfunded);
 }
 
 // show only games that are fully funded
@@ -111,10 +114,13 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let listOfGamesFunded = GAMES_JSON.filter( (game) => {
+        return game.pledged >= game.goal;
+    });
+    console.log(listOfGamesFunded);
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(listOfGamesFunded);
 }
 
 // show all games
@@ -122,7 +128,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -131,7 +137,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
